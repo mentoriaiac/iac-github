cceea25bf7fbd6284afde42d7db382d3330471e9
Rafael Gomes <linux.rafa@gmail.com>
2022-02-06 16:30:26 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "github_teams_members" {
   source       = "github.com/mentoriaiac/iac-modulo-github.git?ref=v0.1.0"
   organization = "mentoriaiac"
-  team_members = ["gomex", "somatorio", "lgfa29", "sebastianwebber", "afonsoaugusto", "edsoncelio", "edsoncelio", "snifbr", "ya-kimura", "Rehzende", "marcelomansur", "lariskovski", "tainara-almeida", "lipenodias", "rodriguezbarreto"]
+  team_members = ["gomex", "somatorio", "lgfa29", "sebastianwebber", "afonsoaugusto", "edsoncelio", "edsoncelio", "snifbr", "ya-kimura", "Rehzende", "marcelomansur", "lariskovski", "tainara-almeida", "lipenodias", "rodriguezbarreto", "jandson-oliveira"]
   team_name    = "moderadores"
 }
